fb95c5abb1a674aea03021bb5299ee83f56d7507
Nico <nicovigil1@gmail.com>
2019-04-17 07:41:19 -0600

    fixes cors issue

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.find_or_create_by(user_params)
-    if user.save && user.name == params["username"]
+    if user.save
       render json: {success: "user successfully created / logged in", id: user.id, username: user.username}, status: 201
     else
       render json: {error: "an error occured"}, status: 401
