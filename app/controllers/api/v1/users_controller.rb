a8d26385e2ccb62379af776a50025d2ebce8544e
Nico <nicovigil1@gmail.com>
2019-04-16 20:57:28 -0600

    edits user registration/login spec

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.find_or_create_by(user_params)
     if user.save
-      render json: {success: "user successfully created / logged in", user: user}, status: 201
+      render json: {success: "user successfully created / logged in", id: user.id, username: user.username}, status: 201
     else
       render json: {error: "an error occured"}, status: 401
     end 
@@ -13,4 +13,4 @@ class Api::V1::UsersController < ApplicationController
   def user_params
     params.permit(:password, :username)
   end
-end 
+end
