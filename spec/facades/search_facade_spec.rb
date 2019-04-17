11feb7da91453e29ea019c107f07dc6af7b0d96e
Nico <nicovigil1@gmail.com>
2019-04-16 18:33:33 -0600

    adds facade for search object serialization

---
+++ spec/facades/search_facade_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "search facade" do
+  it 'can have attributes' do 
+    search = SearchFacade.new
+
+    expect(search.city_id).to eq(0)
+    expect(search.walkers).to eq(0)
+    expect(search.cyclists).to eq(0)
+    expect(search.property_val).to eq(0)
+    expect(search.population).to eq(0)
+  end 
+
+  it 'can generate itself with a series of api calls', :vcr do 
+    search_gen = SearchFacade.gen("denver")
+   
+    expect(search_gen.city_id).to eq("16000US0820000")
+    expect(search_gen.walkers).to eq(16856.0)
+    expect(search_gen.cyclists).to eq(8181.0)
+    expect(search_gen.property_val).to eq(360900.0)
+    expect(search_gen.population).to eq(693060)
+  end
+end 
