18d4440262e5c3144d17ca96e4bca6250780dbeb
Nico <nicovigil1@gmail.com>
2019-04-16 19:22:11 -0600

    minor fix to search facade for fastjson rendering

--- app/facade/search_facade.rb
+++ app/facade/search_facade.rb
@@ -1,8 +1,8 @@
 class SearchFacade
-  attr_reader :city_id, :walkers, :cyclists, :property_val, :population 
+  attr_reader :id, :walkers, :cyclists, :property_val, :population 
 
   def initialize(search_info = Hash.new(0))
-    @city_id = search_info[:city_id]
+    @id = search_info[:id]
     @walkers = search_info[:walkers]
     @cyclists = search_info[:cyclists]
     @property_val = search_info[:property_val]
@@ -12,7 +12,7 @@ class SearchFacade
   def self.gen(city)
     city_data = CityDataService.new
     search_info = Hash.new
-    search_info[:city_id] = city_data.find_city_id(city)
+    search_info[:id] = city_data.find_city_id(city)
     search_info[:walkers] = city_data.find_city_walkers(city)
     search_info[:cyclists] = city_data.find_city_cyclists(city)
     search_info[:property_val] = city_data.median_property_value(city)
