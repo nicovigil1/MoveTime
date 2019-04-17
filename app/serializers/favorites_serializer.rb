04df637a381aabc4b722c2db9cd866739110546a
Nico <nicovigil1@gmail.com>
2019-04-17 08:13:57 -0600

    adds city to serializer

--- app/serializers/favorites_serializer.rb
+++ app/serializers/favorites_serializer.rb
@@ -1,4 +1,4 @@
 class FavoritesSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :city_id, :walkers, :cyclists, :property_val, :population
+  attributes :city_id, :walkers, :cyclists, :property_val, :population, :city
 end
